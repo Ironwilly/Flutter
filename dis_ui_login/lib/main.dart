c81004f55e307c60888c01de83f6135c084bce66
Guillermo <claseswilly@gmail.com>
2022-01-11 22:49:41 +0100

    Casa 11 enero 2022

--- dis_ui_login/lib/main.dart
+++ dis_ui_login/lib/main.dart
@@ -32,25 +32,68 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/path_logo.png'),
-            const Text(
-              'Beautiful,Private Sharing',
-              style: TextStyle(color: Colors.white),
+      body: Column(
+        children: <Widget>[
+          const Padding(padding: EdgeInsets.only(top: 230.0)),
+          Container(
+            child: Center(
+              child: Image.asset(
+                "assets/images/path_logo.png",
+                width: 200.0,
+              ),
             ),
-            TextButton(
+          ),
+          const Text('Beautiful, Private Sharing',
+              style: TextStyle(color: Colors.white54, fontSize: 16)),
+          const Padding(padding: EdgeInsets.only(bottom: 130.0)),
+          Container(
+            child: SizedBox(
+              width: 250,
+              height: 55,
+              child: ElevatedButton(
                 onPressed: () {},
-                style: TextButton.styleFrom(primary: Colors.white),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
                 child: Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 20),
-                )),
-            const Text('ff'),
-          ],
-        ),
+                  "Sign up",
+                  style: TextStyle(
+                      color: Color(0xFFE62F16),
+                      fontSize: 20.00,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 20.0)),
+          const Text('Already have a Path account?',
+              style: TextStyle(color: Colors.white54, fontSize: 14)),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SizedBox(
+            width: 250,
+            height: 55,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFE62F16),
+                  side: BorderSide(color: Colors.white)),
+              child: Text(
+                "Log in",
+                style: TextStyle(
+                    color: Color(0xFFE62F16),
+                    fontSize: 20.00,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(top: 20.00)),
+          Text("By using Path, you agree to Path´s",
+              style: TextStyle(color: Colors.white)),
+          Text(
+            "Term of Use and Privacy Policy",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
       backgroundColor: const Color(0xFFE62F16),
     );
