c36582656b65b4e64e8f3b473ecb670231c4e5bc
Ironwilly <claseswilly@gmail.com>
2022-01-26 12:39:34 +0100

    Clases 26 enero 2022

--- listado_moviedb/lib/pages/movie.dart
+++ listado_moviedb/lib/pages/movie.dart
@@ -16,29 +16,62 @@ class _MovieState extends State<Movie> {
       backgroundColor: Color.fromRGBO(255, 255, 255, 1),
       body: Container(
         margin: EdgeInsets.only(left: 10, bottom: 400),
-        width: 200,
+        width: 400,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Movie, Series, TV Shows...',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+            Row(
+              children: [
+                Container(
+                  child: Image.asset(
+                    "assets/images/emoji.png",
+                    scale: 35,
+                  ),
+                ),
+                Container(
+                  child: Text("      Erik Howel"),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 200),
+                  child: IconButton(
+                      icon: Icon(Icons.connected_tv), onPressed: () {}),
+                )
+              ],
             ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: 200,
-                    height: 40,
-                    margin: EdgeInsets.only(top: 50),
-                    color: Colors.white,
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Search',
-                        border: OutlineInputBorder(),
-                      ),
+            Container(
+              width: 200,
+              child: Text(
+                'Movie, Series, TV Shows...',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 200,
+                          height: 40,
+                          margin: EdgeInsets.only(top: 50),
+                          color: Colors.white,
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Search',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 50, left: 110),
+                          child: IconButton(
+                              icon: Icon(Icons.tune_outlined),
+                              onPressed: () {}),
+                        )
+                      ],
                     ),
                   ),
                 ],
